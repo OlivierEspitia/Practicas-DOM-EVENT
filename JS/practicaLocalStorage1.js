4d35a3915786f0c07f0c0c30609d4a3f90f3fa55
Allan Espitia <espitiaallan@gmail.com>
2023-09-02 19:35:29 -0600

    agregue la pagina principal

--- JS/practicaLocalStorage1.js
+++ JS/practicaLocalStorage1.js
@@ -1,23 +1,23 @@
 
-const inputMail =  document.getElementById("mailID");
-    const inputPassword = document.getElementById("passwordID");
-    const botonAcceder = document.getElementById("acceder");
-    function guardar_totalUsuarios(){
-        const usuarios = [
-              {"correo": "user1@gmail.com", "password": "pass1"},
-              {"correo": "user2@gmail.com", "password": "pass2"},
-              {"correo": "user3@gmail.com", "password": "pass3"},
-              {"correo": "user4@gmail.com", "password": "pass4"},
-              {"correo": "user5@gmail.com", "password": "pass5"},
-              {"correo": "user6@gmail.com", "password": "pass6"},
-              {"correo": "user7@gmail.com", "password": "pass7"},
-              {"correo": "user8@gmail.com", "password": "pass8"},
-              {"correo": "user9@gmail.com", "password": "pass9"},
-              {"correo": "user10@gmail.com", "password": "pass10"}
-            ];
-          localStorage.setItem("data", JSON.stringify(usuarios));
-    }
-    guardar_totalUsuarios();
+const inputMail = document.getElementById("mailID");
+const inputPassword = document.getElementById("passwordID");
+const botonAcceder = document.getElementById("acceder");
+function guardar_totalUsuarios() {
+    const usuarios = [
+        { "correo": "user1@gmail.com", "password": "pass1" },
+        { "correo": "user2@gmail.com", "password": "pass2" },
+        { "correo": "user3@gmail.com", "password": "pass3" },
+        { "correo": "user4@gmail.com", "password": "pass4" },
+        { "correo": "user5@gmail.com", "password": "pass5" },
+        { "correo": "user6@gmail.com", "password": "pass6" },
+        { "correo": "user7@gmail.com", "password": "pass7" },
+        { "correo": "user8@gmail.com", "password": "pass8" },
+        { "correo": "user9@gmail.com", "password": "pass9" },
+        { "correo": "user10@gmail.com", "password": "pass10" }
+    ];
+    localStorage.setItem("data", JSON.stringify(usuarios));
+}
+guardar_totalUsuarios();
 
 botonAcceder.addEventListener('click', () => {
     const correo = inputMail.value;
@@ -25,14 +25,14 @@ botonAcceder.addEventListener('click', () => {
     const misUsuariosEnLocalStorage = JSON.parse(localStorage.getItem("data"));
 
     let coincidencias = misUsuariosEnLocalStorage.some(usuario => {
-        return usuario.correo ===  correo && usuario.password === password;
+        return usuario.correo === correo && usuario.password === password;
     });
-    
-  if(coincidencias){
-    alert("felicidades accediste con exito")
-  } else {
-    alert("tu usuario y contraseña no son los correctos")
-  }
+
+    if (coincidencias) {
+        alert("felicidades accediste con exito")
+    } else {
+        alert("tu usuario y contraseña no son los correctos")
+    }
 });
 
 
